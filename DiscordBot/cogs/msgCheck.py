92db874879d3aba7be0f4596b3baa34acd057cd6
Exabby <teerapatfluck46@gmail.com>
2024-06-17 01:41:20 +0700

    messing around

--- DiscordBot/cogs/msgCheck.py
+++ DiscordBot/cogs/msgCheck.py
@@ -6,16 +6,15 @@ class MsgCheck(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.timeout_duration = 10
+        self.words = ["กามู", "ไกปู", "มา"]
 
-        self.words = ["กามู","ไกปู","มา"]
-        
-    @commands.command(name="addWord" )
+    @commands.command(name="addWord")
     @commands.has_permissions(administrator=True)
     async def add_word(self, ctx, *, message):
         self.words.append(message)
         await ctx.send(f">>> เพิ่มอันนี้เข้าไปในคลังคำห้ามใช้แล้ว: {message}")
     
-    @commands.command(name="removeWord" )
+    @commands.command(name="removeWord")
     @commands.has_permissions(administrator=True)
     async def remove_word(self, ctx, *, message):
         if message in self.words:
@@ -23,27 +22,35 @@ class MsgCheck(commands.Cog):
             await ctx.send(f">>> ลบอันนี้ออกจากคลังคำห้ามใช้แล้ว: {message}")
         else:
             await ctx.send(f">>> ไม่พบคำนี้ในคลังคำห้ามใช้: {message}")
-            
+    
     @commands.command(name="wordList")
     async def word_list(self, ctx):
         word_string = ", ".join(self.words)
         await ctx.send(f">>> คำที่อยู่ในคลังคำห้ามใช้: [{word_string}]")
-        
     
     @commands.Cog.listener()
     async def on_message(self, message):
-        # Check if the message content meets your criteria
+        if message.author == self.bot.user:
+            return  # Ignore messages from the bot itself
+        
+        # Check if the message content contains any forbidden words
         if message.content in self.words:
             if message.author.top_role > message.guild.me.top_role:
-                await message.channel.send(">>> "+ message.content +"อะไ- ขอโทษครับพี่ ลืมดูคนพิมพ์")
+                await message.channel.send(">>> " + message.content + " อะไ- ขอโทษครับพี่ ลืมดูคนพิมพ์")
                 return
             
-            # Perform some action or send a response
-            await message.channel.send(">>> "+message.content + "ไร เอาไป " + str(self.timeout_duration) + " วิ")
+            await message.channel.send(">>> " + message.content + " ไร เอาไป " + str(self.timeout_duration) + " วิ")
+        
+            # If the user is in a voice channel, disconnect them
+            if message.author.voice is not None:
+                await message.author.move_to(None)
 
-            # Define a role for timeout
-            timeout_role = discord.utils.get(">>> "+ message.guild.roles, name="Timeout")
-
+            # Retrieve the timeout role
+            timeout_role = discord.utils.get(message.guild.roles, name="Timeout")
+            if timeout_role is None:
+                await message.channel.send(">>> ไม่เจอยศ 'Timeout' สร้างก่อนนะพี่")
+                return
+            
             # Store the member's current roles
             current_roles = message.author.roles
             
@@ -52,18 +59,20 @@ class MsgCheck(commands.Cog):
                 if role != message.guild.default_role:  # Don't remove the default role
                     await message.author.remove_roles(role)
             
-            # Add the role to the member
+            
+            # Add the timeout role to the member
             await message.author.add_roles(timeout_role)
-
+            
+            
             # Wait for the duration of the timeout, then remove the role
             await asyncio.sleep(self.timeout_duration)
             
             await message.author.remove_roles(timeout_role)
 
-             # Add the original roles back to the member
+            # Add the original roles back to the member
             for role in current_roles:
                 if role != message.guild.default_role:  # Don't add the default role
                     await message.author.add_roles(role)
-            
+
 async def setup(bot):
     await bot.add_cog(MsgCheck(bot))
